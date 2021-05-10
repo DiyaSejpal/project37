9209bd2958475d93be9b938594b0c8dadac13f28
DiyaSejpal <76207044+DiyaSejpal@users.noreply.github.com>
2021-05-10 15:45:01 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,74 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+
+    //write code to change the background color here
+background("blue");    
+
+    //write code to show a heading for showing the result of Quiz
+    var title1 = createElement("h1");
+    title1.html("Result of the quiz");
+    title1.position(350,0);
+
+    //call getContestantInfo( ) here
+    Contestant.getPlayerInfo();
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants !== undefined){
+      fill("yellow");
+      textSize(20);
+      text(" NOTE : User who has not answered correctly is in red color",130,230);
+
+      
+    //write code to highlight contest who answered correctly
+    for (var plr in allContestants){
+      var correctAns = "2";
+      if(correctAns=== allContestants[plr].answer){
+        fill("green");
+        text(allContestants[plr].name+" : "+allContestants[plr].answer ,150,270);
+      }else {
+        fill("red");
+        text(allContestants[plr].name+" : "+allContestants[plr].answer ,150,300);
+      }
+    }
+    }
+
+    
+
+
+    
+  }
+
+
+
+}
